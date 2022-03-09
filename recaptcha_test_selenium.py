b332844ee08798f08d2cfefe35db6a52e043422e
asb1512 <andrew@olympicmedia.com>
2022-03-09 10:44:14 -0500

    added necessary imports and main logic functino

--- recaptcha_test_selenium.py
+++ recaptcha_test_selenium.py
@@ -1,5 +1,12 @@
 import tkinter
 import subprocess
+from turtle import width
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as ExpectedConditions
+from selenium.webdriver.support.ui import Select
 
 # setting up native window
 root = Tk()
@@ -19,3 +26,16 @@ def open_chrome():
       "--user-data-dir=/Users/andrewbourgeois/Library/Application Support/Google/Chrome/Default"
     ]
   )
+
+  # main program logic
+  def run_recaptcha_test():
+
+    # Chrome options
+    options = Options()
+
+  # start button
+  start_app_button = tkinter.Button(
+    root, width=200, bg="green", fg="white", text="Start", 
+  )
+
+  root.mainLoop()
